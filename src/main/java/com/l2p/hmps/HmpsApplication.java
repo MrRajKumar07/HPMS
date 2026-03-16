141cef8a71e2685c892d3f84464cc08b702f1d32
MrRajKumar07 <rajkumar951ce@gmail.com>
2026-03-16 14:48:45 +0530

    Phase 1: Initial setup, shared files, and Auth foundation

---
+++ src/main/java/com/l2p/hmps/HmpsApplication.java
@@ -0,0 +1,13 @@
+package com.l2p.hmps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HmpsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/l2p/hmps/HmpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.l2p.hmps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HmpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
